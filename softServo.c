bcae2605e2bdb9a4f2822a37f9a1a37febac38e4
imaicom <i@okako.com>
2016-05-08 22:03:17 +0900

    Update softServo.c

--- softServo.c
+++ softServo.c
@@ -1,30 +1,18 @@
 #include <stdio.h>
-#include <errno.h>
-#include <string.h>
-
+#include <unistd.h>
 #include <wiringPi.h>
 #include <softServo.h>
 
 int main () {
+	
 	wiringPiSetup();
-	softServoSetup (0, -1 , -1 , -1 , -1 , -1 , -1 , -1) ;
-	softServoWrite (0,  0) ;
+	softServoSetup (5, -1 , -1 , -1 , -1 , -1 , -1 , -1) ;
+	softServoWrite (5,  0) ;
 	sleep(1);
-	softServoWrite (0,  500) ;
+	softServoWrite (5,  500) ;
 	sleep(1);
-	softServoWrite (0,  100) ;
+	softServoWrite (5,  100) ;
 	sleep(1);
-	softServoWrite (0,  500) ;
+	softServoWrite (5,  500) ;
 	sleep(1);
-
-/*
-	softServoWrite (1, 1000) ;
-	softServoWrite (2, 1100) ;
-	softServoWrite (3, 1200) ;
-	softServoWrite (4, 1300) ;
-	softServoWrite (5, 1400) ;
-	softServoWrite (6, 1500) ;
-	softServoWrite (7, 2200) ;
-*/
-
 }
