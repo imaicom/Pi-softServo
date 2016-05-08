88a55683ae1ee01a626004ed5f0743167dc09e80
imaicom <i@okako.com>
2016-05-08 22:10:33 +0900

    Update softServo.c

--- softServo.c
+++ softServo.c
@@ -6,7 +6,7 @@
 int main () {
 	
 	wiringPiSetup();
-	softServoSetup (5, -1 , -1 , -1 , -1 , -1 , -1 , -1) ;
+	softServoSetup (5, -1 , -1 , -1 , -1 , -1 , -1 , -1) ; // Pin number of each of the servo, "-1" Missing servo
 	softServoWrite (5,  0) ;
 	sleep(1);
 	softServoWrite (5,  500) ;
