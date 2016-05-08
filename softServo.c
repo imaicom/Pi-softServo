e06edd3ccc887ab708c3201c6a46c51fb3e114c1
imaicom <i@okako.com>
2016-05-08 22:00:26 +0900

    Create softServo.c

---
+++ softServo.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+
+#include <wiringPi.h>
+#include <softServo.h>
+
+int main () {
+	wiringPiSetup();
+	softServoSetup (0, -1 , -1 , -1 , -1 , -1 , -1 , -1) ;
+	softServoWrite (0,  0) ;
+  sleep(1);
+	softServoWrite (0,  500) ;
+  sleep(1);
+	softServoWrite (0,  100) ;
+  sleep(1);
+	softServoWrite (0,  500) ;
+  sleep(1);
+
+/*
+	softServoWrite (1, 1000) ;
+	softServoWrite (2, 1100) ;
+	softServoWrite (3, 1200) ;
+	softServoWrite (4, 1300) ;
+	softServoWrite (5, 1400) ;
+	softServoWrite (6, 1500) ;
+	softServoWrite (7, 2200) ;
+*/
+
+}
