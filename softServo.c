6c6f64041b51d36995ac067ca6ba29b382fe05c3
imaicom <i@okako.com>
2016-07-27 07:52:42 +0900

    Update softServo.c

--- softServo.c
+++ softServo.c
@@ -10,6 +10,7 @@ int main () {
 	wiringPiSetup();
 	softServoSetup (5, -1 , -1 , -1 , -1 , -1 , -1 , -1) ; // Pin number of each of the servo, "-1" Missing servo
 	// The pin number details -> https://github.com/imaicom/Pi-pin-Finder/blob/master/raspi-pin.pdf
+	// You can control the servo motor up to 8 in this program.
 	softServoWrite (5,  0) ;
 	sleep(1);
 	softServoWrite (5,  500) ;
