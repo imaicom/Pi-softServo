63f95e1b644278aa8f02c629b86743ca9620eb75
makoto imai <i@okako.com>
2017-07-26 20:27:12 +0900

    new file:   softServo 	modified:   softServo.c 	new file:   softServo.o

--- softServo.c
+++ softServo.c
@@ -1,4 +1,4 @@
-// cc -o softServo softServo.c  -lwiringPi -lm
+// cc -o "softServo" "softServo.c" ~/wiringPi/wiringPi/softServo.c -IwiringPi/wiringPi -lwiringPi -lm -lpthread
 
 #include <stdio.h>
 #include <unistd.h>
@@ -8,15 +8,17 @@
 int main () {
 	
 	wiringPiSetup();
-	softServoSetup (5, -1 , -1 , -1 , -1 , -1 , -1 , -1) ; // Pin number of each of the servo, "-1" Missing servo
+	softServoSetup (23, -1 , -1 , -1 , -1 , -1 , -1 , -1) ; // Pin number of each of the servo, "-1" Missing servo
 	// The pin number details -> https://github.com/imaicom/Pi-pin-Finder/blob/master/raspi-pin.pdf
 	// You can control the servo motor up to 8 in this program.
-	softServoWrite (5,  0) ;
-	sleep(1);
-	softServoWrite (5,  500) ;
-	sleep(1);
-	softServoWrite (5,  100) ;
-	sleep(1);
-	softServoWrite (5,  500) ;
-	sleep(1);
+	softServoWrite (23,  0) ;
+	sleep(3);
+	softServoWrite (23,  500) ;
+	sleep(3);
+	softServoWrite (23,  1000) ;
+	sleep(3);
+	softServoWrite (23,  500) ;
+	sleep(3);
+	softServoWrite (23,  0) ;
+	sleep(3);
 }
